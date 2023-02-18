02e1812dced540b1955d0d330c72e7a2db8dd59e
Prabhat-Bhanderi <prabhat.bhanderi@gmail.com>
2023-02-18 22:15:58 +0530

    node/express tutorials

--- myCode/4-express-app.js
+++ myCode/4-express-app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const app = express();
 
 app.use(express.static('./public'))
 
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Page not Found</h1>')
