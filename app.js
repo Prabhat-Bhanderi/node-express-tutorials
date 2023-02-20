09b49e24e385a9419e0f948b497b5ae157eec588
Prabhat-Bhanderi <prabhat.bhanderi@gmail.com>
2023-02-20 14:09:41 +0530

    node/express tutorials

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 4545;
+const port = 4545
+let { people } = require('./data')
 
-const logger = require('./middleware/logger');
-const authorize = require('./middleware/authorize');
-
-app.use([logger,authorize]);
-// below code apply middleware after all routs of api
-// app.use('/api',logger);
+//static assets
+app.use(express.static('./methods-public'))
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome home ${req.user.name}</h1>`)
+    res.send('hii')
 })
-app.get('/about', (req, res) => {
-    res.send('about')
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success:true,data:people})
 })
-app.get('/api/products', logger, (req, res) => {
-    res.send('Products API')
+app.post('/login',(req,res)=>{
+    res.send('Post')
 })
-app.get('/api/items', logger, (req, res) => {
-    res.send('Items API')
-})
+
 
 app.listen(port)
